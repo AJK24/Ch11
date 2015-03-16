60c7c297a3d0dd83b8db320aff1148590de6edb0
Andrew <akv9509@saintjoe.edu>
2015-03-16 12:52:34 -0400

    Another nonworking update

--- testgraphs.js
+++ testgraphs.js
@@ -1,14 +1,16 @@
 load('Graphs.js');
 
-g = new Graph(9);
+
+
+g = new Graph(5);
 g.addEdge(0,1);
-g.addEdge(1,2);
-g.addEdge(2,3);
-g.addEdge(3,5);
-g.addEdge(5,7);
-g.addEdge(7,6);
-g.addEdge(6,4);
-g.addEdge(4,8);
+g.addEdge(0,2);
+g.addEdge(1,3);
+g.addEdge(2,4);
+g.showGraph();
+g.dfs(0);
+
+
 
 var houses = ['foundation', 'walls', 'roof', 'drywall', 'furnature', 'decorations','plumbing', 'eletrical',  'family'];
 
