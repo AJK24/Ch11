3ee532e30bc7e61e73043ce7cfdfa2eb56d8fc8f
Andrew <akv9509@saintjoe.edu>
2015-03-17 21:38:49 -0400

    Issues with testgraph.. Nothing is being added, it is all undefined still I think it is in the show graph of newgraph.js

--- testgraphs.js
+++ testgraphs.js
@@ -8,12 +8,12 @@ g.addEdge(0,2);
 g.addEdge(1,3);
 g.addEdge(2,4);
 g.showGraph();
-g.dfs(0);
+//g.dfs(0);
 
 
 
-var houses = ['foundation', 'walls', 'roof', 'drywall', 'furnature', 'decorations','plumbing', 'eletrical',  'family'];
+//var houses = ['foundation', 'walls', 'roof', 'drywall', 'furnature', 'decorations','plumbing', 'eletrical',  'family'];
 
-g.showGraph();
-print();
-g.topSort();
+//g.showGraph();
+//print();
+//g.topSort();
